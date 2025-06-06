16dda3c1d648c9e833dcef7946198fe316e9f5ef
Umeshwar007 <151039459+Umeshwar007@users.noreply.github.com>
2025-06-06 16:39:39 +0530

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -13,7 +13,7 @@ import {
 } from 'react-icons/fa';
 import { inputBase, iconClass } from '../../assets/dummydata';
 
-const url = 'http://localhost:4000'
+const url = 'https://zwiggyy.onrender.com'
 
 const Login = ({ onLoginSuccess, onClose }) => {
   const [formData, setFormData] = useState({
