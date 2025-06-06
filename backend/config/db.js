91dd18691e8c19716598b690c8a68c6b9b8d0aa4
Umeshwar007 <151039459+Umeshwar007@users.noreply.github.com>
2025-06-06 17:16:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://umeshwar788:umeshwar007@cluster0.keliu8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect('mongodb+srv://umeshwar788:<db_password>@cluster0.keliu8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         .then(() => console.log('DB CONNECTED'));
 }
 
