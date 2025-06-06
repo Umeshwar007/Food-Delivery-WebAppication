3d31d9c08cf304bee8e0946b3ccc966654de352f
Umeshwar007 <151039459+Umeshwar007@users.noreply.github.com>
2025-06-06 16:28:16 +0530

    Update AddItems.jsx

--- admin/src/components/AddItems/AddItems.jsx
+++ admin/src/components/AddItems/AddItems.jsx
@@ -54,7 +54,7 @@ const AddItems = () => {
         payload.append(key, val);
       });
       const res = await axios.post(
-        'http://localhost:4000/api/items',
+        'https://zwiggyy.onrender.com/api/items',
         payload,
         { headers: { 'Content-Type': 'multipart/form-data' } }
       );
